70ed9fd0edd9edb4551573a3b6fb219471a0fd47
Nick Young <nick.young@auckland.ac.nz>
2021-05-20 10:50:36 +1200

    exdata format

--- merge.py
+++ merge.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import logging
 import time
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,8 +14,14 @@ start = time.time()
 parser = argparse.ArgumentParser(description="Melanoma data tool")
 parser.add_argument('-f', '--filter', help='which columns and values to filter by')
 parser.add_argument('-o', '--outfile', help='Output file', default="out.ipdata")
-parser.add_argument('-of', '--output-format', help='Output format', default="ipdata")
+parser.add_argument('-of', '--output-format', help='Output format', choices=["ipdata", "exdata"], default="ipdata")
 args = parser.parse_args()
+
+# Set correct file extensions
+if args.output_format == "exdata":
+    args.outfile = Path(args.outfile).with_suffix(".exdata")
+elif args.output_format == "ipdata":
+    args.outfile = Path(args.outfile).with_suffix(".ipdata")
 
 # Load data
 all_sites = pd.read_excel("data/all_melanoma_sites.xlsx")
@@ -48,12 +55,28 @@ logging.info(f"Data merged in {round(time.time() - start, 4)}s")
 
 merged.index = merged.index + 1
 merged["scale"] = 1.0
-cols = ["cmgui_x", "cmgui_y", "cmgui_z"]
-for col in cols:
-    merged[col] = merged[col].apply(lambda x: '%.4f' % x)
 
-if args.output_format == "ipdata":
-    with open(args.outfile, 'w') as file:
+cols = ["cmgui_x", "cmgui_y", "cmgui_z", "count"]
+
+# Write out
+with open(args.outfile, 'w') as file:
+    if args.output_format == "ipdata":
         file.write('Data file\n')
+        for col in cols:
+            merged[col] = merged[col].apply(lambda x: '%.4f' % x)
         merged[["cmgui_x", "cmgui_y", "cmgui_z", "count", "scale", "scale", "scale", "scale"]].to_csv(file, header=False, sep='\t')
-    logging.info(f"{args.outfile} written")
+    elif args.output_format == "exdata":
+        file.write(" Group name: data\n" +
+                        " #Fields=2\n" +
+                        " 1) coordinates, coordinate, rectangular cartesian, #Components=3\n" +
+                        "   x.  Value index= 1, #Derivatives=0\n" +
+                        "   y.  Value index= 2, #Derivatives=0\n" +
+                        "   z.  Value index= 3, #Derivatives=0\n" +
+                        " 2) count, field, rectangular cartesian, #Components=1\n" +
+                        "   1.  Value index= 4, #Derivatives=0\n")
+        for col in cols:
+            merged[col] = merged[col].apply(lambda x: '%.5e' % x)
+        for index, row in merged.iterrows():
+            file.write(f" Node:  {index}\n    {row['cmgui_x']}  {row['cmgui_y']}  {row['cmgui_z']}\n    {row['count']}\n")
+
+logging.info(f"{args.outfile} written")
