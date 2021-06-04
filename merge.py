a6e2dced5963638f17a1bab4fbf58b9015273868
Nick Young <nick.young@auckland.ac.nz>
2021-06-04 15:38:16 +1200

    write both formats, reworking flow

--- merge.py
+++ merge.py
@@ -14,15 +14,8 @@ start = time.time()
 parser = argparse.ArgumentParser(description="Melanoma data tool")
 parser.add_argument('-f', '--filter', help='Which columns and values to filter by')
 parser.add_argument('-o', '--outfile', help='Output file', default="out.ipdata")
-parser.add_argument('-of', '--output-format', help='Output format', choices=["ipdata", "exdata"], default="ipdata")
 parser.add_argument('-n', '--normalise', help='Calculate normalised frequency as percentage (divide number of patients with drainage to specified node field by total number of patients at site)', action='store_true')
 args = parser.parse_args()
-
-# Set correct file extensions
-if args.output_format == "exdata":
-    args.outfile = Path(args.outfile).with_suffix(".exdata")
-elif args.output_format == "ipdata":
-    args.outfile = Path(args.outfile).with_suffix(".ipdata")
 
 # Load data
 all_sites = pd.read_excel("data/all_melanoma_sites.xlsx")
@@ -38,50 +31,39 @@ logging.info(f"Data loaded in {round(time.time() - start, 4)}s")
 patients = patients.join(followup, rsuffix='_followup')
 original_patients = patients.copy()
 
-if args.filter:
-    filters = args.filter.split("&")
-    for filter in filters:
-        k, v = filter.split("=")
-        original_length = len(patients)
-        try:
-            if k == "Node Fields":
-                patients = patients[patients[k].str.contains(rf'\b{v}\b')]
-            else:
-                patients = patients[patients[k] == v]
-        except KeyError:
-            logging.error(f"Column {k} not known: possible columns to choose from: {sorted(patients.columns.tolist())}")
-            exit(1)
-        logging.info(f"After applying filter {filter}, reduced dataset size from {original_length} to {len(patients)}")
+def calc():
+    key = ["Map", "X", "Y"]
+    if args.normalise:
+        # Patients with melanoma at each site draining to selected node field
+        numerator = patients.groupby(key).size().reset_index(name='selected_node')
+        # All patients with melanoma at each site, any node field
+        denominator = original_patients.groupby(key).size().reset_index(name='any_node')
+        grouped_patients = numerator.merge(denominator, left_on=key, right_on=key, how="right").fillna({'selected_node': 0})
+        grouped_patients["count"] = grouped_patients["selected_node"] / grouped_patients["any_node"] * 100 # percentage
+    else:
+        grouped_patients = patients.groupby(key).size().reset_index(name='count')
 
-key = ["Map", "X", "Y"]
-if args.normalise:
-    # Patients with melanoma at each site draining to selected node field
-    numerator = patients.groupby(key).size().reset_index(name='selected_node')
-    # All patients with melanoma at each site, any node field
-    denominator = original_patients.groupby(key).size().reset_index(name='any_node')
-    grouped_patients = numerator.merge(denominator, left_on=key, right_on=key, how="right").fillna({'selected_node': 0})
-    grouped_patients["count"] = grouped_patients["selected_node"] / grouped_patients["any_node"] * 100 # percentage
-else:
-    grouped_patients = patients.groupby(key).size().reset_index(name='count')
+    # Merge all site information
+    start = time.time()
+    merged = grouped_patients.merge(all_sites, left_on=key, right_on=["Body map #", "X", "Y"], how="right").fillna({'count': 0})
+    logging.info(f"Data merged in {round(time.time() - start, 4)}s")
 
-# Merge all site information
-start = time.time()
-merged = grouped_patients.merge(all_sites, left_on=key, right_on=["Body map #", "X", "Y"], how="right").fillna({'count': 0})
-logging.info(f"Data merged in {round(time.time() - start, 4)}s")
+    merged.index = merged.index + 1
+    merged["scale"] = 1.0
+    return merged
 
-merged.index = merged.index + 1
-merged["scale"] = 1.0
-
-cols = ["cmgui_x", "cmgui_y", "cmgui_z", "count"]
-
-# Write out
-with open(args.outfile, 'w') as file:
-    if args.output_format == "ipdata":
+def save(merged, filename = args.outfile):
+    cols = ["cmgui_x", "cmgui_y", "cmgui_z", "count"]
+    # Write out
+    filename = Path(filename).with_suffix(".ipdata")
+    with open(filename, "w") as file:
         file.write('Data file\n')
         for col in cols:
             merged[col] = merged[col].apply(lambda x: '%.4f' % x)
         merged[["cmgui_x", "cmgui_y", "cmgui_z", "count", "scale", "scale", "scale", "scale"]].to_csv(file, header=False, sep='\t')
-    elif args.output_format == "exdata":
+        logging.info(f"{filename} written")
+    filename = Path(filename).with_suffix(".exdata")
+    with open(filename, "w") as file:
         file.write(" Group name: data\n" +
                         " #Fields=2\n" +
                         " 1) coordinates, coordinate, rectangular cartesian, #Components=3\n" +
@@ -94,5 +76,24 @@ with open(args.outfile, 'w') as file:
             merged[col] = merged[col].apply(lambda x: '%.5e' % x)
         for index, row in merged.iterrows():
             file.write(f" Node:  {index}\n    {row['cmgui_x']}  {row['cmgui_y']}  {row['cmgui_z']}\n    {row['count']}\n")
+        logging.info(f"{filename} written")
 
-logging.info(f"{args.outfile} written")
+if args.filter:
+    filters = args.filter.split("&")
+    for filter in filters:
+        k, v = filter.split("=")
+        original_length = len(patients)
+        try:
+            if k == "Node Fields":
+                for nodeField in v.split(","):
+                    patients = original_patients[original_patients[k].str.contains(rf'\b{nodeField}\b', na=False)]
+                    save(calc(), nodeField)
+            else:
+                patients = patients[patients[k] == v]
+                save(calc())
+        except KeyError:
+            logging.error(f"Column {k} not known: possible columns to choose from: {sorted(patients.columns.tolist())}")
+            exit(1)
+        logging.info(f"After applying filter {filter}, reduced dataset size from {original_length} to {len(patients)}")
+else:
+    save(calc())
